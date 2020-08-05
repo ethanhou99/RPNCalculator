008109d8bfa81fe593791a24017639678a74ae53
Yicun Hou <yicunhou@bu.edu>
2020-08-04 21:10:07 -0400

    Main class of the calculator.

--- src/main/java/rpn/RpnMain.java
+++ src/main/java/rpn/RpnMain.java
@@ -4,5 +4,15 @@ import java.util.Scanner;
 
 public class RpnMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        RpnCalculator rpn = new RpnCalculator();
+        System.out.println("Please input operations, input 'exit' to exit program：");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            rpn.calculate(input);
+        }
     }
 }
