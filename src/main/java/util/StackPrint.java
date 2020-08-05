515fcbab2a0441e74f1ea4fb6e8a765b15e8cc52
Yicun Hou <yicunhou@bu.edu>
2020-08-04 21:18:33 -0400

    Function to print out the stack content.

---
+++ src/main/java/util/StackPrint.java
@@ -0,0 +1,29 @@
+package util;
+
+import java.util.LinkedList;
+
+public class StackPrint {
+    LinkedList<Double> numStack;
+    private static final String REDUNDANT_ZEROS = "\\.*0*$";
+    public StackPrint(LinkedList<Double> numStack) {
+        this.numStack = numStack;
+    }
+
+    public void print() {
+        System.out.print("stack: ");
+        if (numStack.size() > 0) {
+            for (int i = numStack.size() - 1; i >= 0; i--) {
+                Double num = numStack.get(i);
+                if (num % 1 == 0) {
+                    System.out.print(num.intValue());
+                } else {
+                    System.out.print(String.format("%.10f", num).replaceAll(REDUNDANT_ZEROS, ""));
+                }
+                if (i > 0) {
+                    System.out.print(" ");
+                }
+            }
+        }
+        System.out.println();
+    }
+}
