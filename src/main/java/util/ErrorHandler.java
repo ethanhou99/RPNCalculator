1170bd39bf756f05f4fb1d1a929703e6888f123c
Yicun Hou <yicunhou@bu.edu>
2020-08-04 21:31:23 -0400

    Upgrade the grammar of the error printer.

--- src/main/java/util/ErrorHandler.java
+++ src/main/java/util/ErrorHandler.java
@@ -11,17 +11,22 @@ public class ErrorHandler {
     }
 
     public void print(List<String> failedNum) {
-        if (failedNum.size() > 0) {
+        if (failedNum.size() > 1) {
             System.out.print("the ");
             for (int i = 0; i < failedNum.size(); i++) {
                 System.out.print(failedNum.get(i));
                 if (i < failedNum.size() - 2) {
                     System.out.print(", ");
                 } else if (i == failedNum.size() - 2) {
-                    System.out.print(", and ");
+                    if (failedNum.size() > 2) {
+                        System.out.print(",");
+                    }
+                    System.out.print(" and ");
                 }
             }
             System.out.println(" were not pushed on to the stack due to the previous error");
+        } else if (failedNum.size() == 1) {
+            System.out.println("the " + failedNum.get(0) + " was not pushed on to the stack due to the previous error");
         }
     }
 }
